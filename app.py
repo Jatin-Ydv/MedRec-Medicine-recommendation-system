25f86a872c75b56fc57a2ee17ae75f4603e660a0
Jatin Yadav <jatinydv299@gmail.com>
2024-08-16 14:54:01 +0530

    adding validation scripts

--- app.py
+++ app.py
@@ -50,12 +50,15 @@ symptom_dict = {column: idx for idx, column in enumerate(X.columns)}
 
 def get_predicted_value(patient_symptoms):
     input_vector=np.zeros(len(symptom_dict))
-    
+
     for item in patient_symptoms:
-        
+        if item not in symptom_dict:
+            return None
         input_vector[symptom_dict[item]]=1
     
     return disease_list[svc.predict([input_vector])[0]]
+
+
 app=Flask(__name__)
 
 
@@ -72,21 +75,29 @@ def index():
 
 def predict():
     if request.method=='POST':
-        print('inside post')
+        
         symptoms=request.form.get('symptoms')
         
-
+        
         user_symptoms=[s.strip() for s in symptoms.split(',')]
+        
         # remove extra characters, if any
         user_symptoms=[sym.strip("[]' ") for sym in user_symptoms]
-        
+            
         pro_user_symptoms=[inp.lower().replace(' ', '_') for inp in user_symptoms]
         predicted_disease=get_predicted_value(pro_user_symptoms)
-
-        des,pre,med,die,wrkout=helper(predicted_disease)
         
-        return render_template('index.html',predicted_disease=predicted_disease,dis_des=des,dis_pre=pre[0],
-                               dis_med=med,dis_wrkout=wrkout,dis_diet=die,user_symptoms=user_symptoms
+        if predicted_disease is None:
+            error_message="Your symptoms are either too generic or outside the scope of MedRec!"
+            des,pre,med,die,wrkout=None,None,None,None,None
+            dis_pre=None
+        else:
+            des,pre,med,die,wrkout=helper(predicted_disease)
+            dis_pre=pre[0]
+            error_message=None
+        return render_template('index.html',predicted_disease=predicted_disease,dis_des=des,dis_pre=dis_pre,
+                               dis_med=med,dis_wrkout=wrkout,dis_diet=die,user_symptoms=user_symptoms,
+                               error_message=error_message
                                )
     
     
